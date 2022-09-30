da3e9e23a1a4a4e506890bac3a8e93ac1bf93632
Vaibhav Singh Chandel <vaibhav24k@gmail.com>
2022-09-30 10:08:24 +0530

    naxre

--- BinarySearchQuestions/src/searchRange.java
+++ BinarySearchQuestions/src/searchRange.java
@@ -1,11 +1,22 @@
 //https://leetcode.com/problems/find-first-and-last-position-of-element-in-sorted-array/
 package BinarySearchQuestions.src;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class searchRange {
     public static void main(String[] args) {
+        System.out.println(Arrays.toString(cmp()));
 
     }
-    static int[] searchRange(int[] nums, int target){
-        
+    static int[] cmp(){
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int temp = a;
+        a = b;
+        b = temp;
+        int[] solve = {a,b};
+        return solve;
     }
 }
