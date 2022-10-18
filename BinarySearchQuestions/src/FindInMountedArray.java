cc57b280b04265d3c3349f5d84e28453293c0d18
Vaibhav Singh Chandel <vaibhav24k@gmail.com>
2022-10-18 15:47:30 +0530

    sqrt(n) by recursion won't work for 2147395599

---
+++ BinarySearchQuestions/src/FindInMountedArray.java
@@ -0,0 +1,27 @@
+//package BinarySearchQuestions.src;
+//
+//public class FindInMountedArray {
+//    public static void main(String[] args) {
+//
+//    }
+//
+//   static int search(int[] arr, int target) {
+//        int pivot = findPivot(arr);
+//   }
+//
+//    public int peakIndexInMountainArray (int[] arr) {
+//        int start = 0;
+//        int end = arr.length - 1;
+//
+//        while (start < end) {
+//            int mid = start + (end - start) / 2;
+//
+//            if (arr[mid] > arr[mid + 1]) {
+//                end = mid;
+//            } else {
+//                start = mid + 1;
+//            }
+//        }
+//        return  start;
+//    }
+//}
--- BinarySearchQuestions/src/sqrtnum.java
+++ BinarySearchQuestions/src/sqrtnum.java
@@ -1,14 +1,21 @@
-//package BinarySearchQuestions.src;
-//
-//public class sqrtnum {
-//    public static void main(String[] args) {
-//        int n = 36;
-//        //System.out.println(sqrt(n));
-//    }
-//    static float sqrt(int n){
-//        int start = 0; int end = n;
-//        int mid;
-//
-//        doub
-//    }
-//}
+package BinarySearchQuestions.src;
+
+public class sqrtnum {
+    public static void main(String[] args) {
+        int n = 5;
+        System.out.println(sqrt(n));
+    }
+    static int sqrt(int n){
+        int mid = 0;
+        int start = 0; int end = n;
+        while (start <= end) {
+            mid = start + (end - start) / 2;
+            if (mid * mid == n) return mid;
+
+            if (mid * mid > n) end = mid - 1;
+
+            else start = mid + 1;
+        }
+        return sqrt(n - 1);
+    }
+}
