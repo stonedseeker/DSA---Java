21b64568c78d788991315d406198ff8e1184e470
stonedseeker <vaibhav24k@gmail.com>
2023-07-23 02:36:29 +0530

    testing

--- Recursion/src/Backtracking/NKnights.java
+++ Recursion/src/Backtracking/NKnights.java
@@ -1,4 +1,11 @@
 package Backtracking;
 
 public class NKnights {
+    public static void main(String[] args) {
+
+    }
+
+    static void NKnights(boolean[] board, int r, int c, int knights){
+        
+    }
 }
